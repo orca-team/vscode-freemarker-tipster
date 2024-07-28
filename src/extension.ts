3631fb2edbc636fc2f5dd4820b60a6765e22c244
Orchard <orchardyz@outlook.com>
2024-07-28 14:32:47 +0800

    chore: update eslint configuration

--- src/extension.ts
+++ src/extension.ts
@@ -7,12 +7,6 @@ import provideMacroCompletionItems from "./provideMacroCompletionItems";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-freemarker-tipster" is now active!'
-  );
-
   const ftlDocumentSelector: vscode.DocumentSelector = {
     language: "ftl",
     scheme: "file",
